c10d9dec47aaf61c7d5d0899eab79b150cb76ad2
MahiHailu <hail.mulat@gmail.com>
2023-08-24 01:18:57 -0700

    monty

---
+++ error_2.c
@@ -0,0 +1,71 @@
+#include "monty.h"
+
+int short_stack_error(unsigned int line_number, char *op);
+int div_error(unsigned int line_number);
+int pop_error(unsigned int line_number);
+int div_error(unsigned int line_number);
+int pchar_error(unsigned int line_number, char *message);
+
+/**
+ * pop_error - prints pop error msg to empty stacks.
+ * @line_number: line num in script where error occurred.
+ * Return: (EXIT_FAILURE) always.
+ */
+
+int pop_error(unsigned int line_number)
+{
+	fprintf(stderr, "L%u: can't pop an empty stack\n", line_number);
+	return (EXIT_FAILURE);
+}
+
+/**
+ * pint_error - prints  error msg for empty stacks.
+ * @line_number: line num in monty bytecodes file where error occurred.
+ *
+ * Return: (EXIT_FAILURE) always.
+ */
+int pint_error(unsigned int line_number)
+{
+	fprintf(stderr, "L%d: can't pint, stack empty\n", line_number);
+	return (EXIT_FAILURE);
+}
+
+/**
+ * short_stack_error - prints monty math func error msg.
+ *                     for stacks or queues smaller than two nodes.
+ * @line_number: line num in monty bytecodes file where error occurred.
+ * @op: operation where the error occurred.
+ *
+ * Return: (EXIT_FAILURE) always.
+ */
+int short_stack_error(unsigned int line_number, char *op)
+{
+	fprintf(stderr, "L%u: can't %s, stack too short\n", line_number, op);
+	return (EXIT_FAILURE);
+}
+
+/**
+ * div_error - Prints division error messages for division by 0.
+ * @line_number: Line number in Monty bytecodes file where error occurred.
+ *
+ * Return: (EXIT_FAILURE) always.
+ */
+int div_error(unsigned int line_number)
+{
+	fprintf(stderr, "L%u: division by zero\n", line_number);
+	return (EXIT_FAILURE);
+}
+
+/**
+ * pchar_error - prints pchar error mssg to empty stacks.
+ *               empty stacks and non-char values.
+ * @line_number: line num in monty bytecodes file where error occurred.
+ * @message: the corresponding error message to print.
+ *
+ * Return: (EXIT_FAILURE) always.
+ */
+int pchar_error(unsigned int line_number, char *message)
+{
+	fprintf(stderr, "L%u: can't pchar, %s\n", line_number, message);
+	return (EXIT_FAILURE);
+}
